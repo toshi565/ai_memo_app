1b1558e04c0e9f7de44c85560355784ce227d39d
toshi565 <ube.toshi.13@gmail.com>
2025-09-07 12:01:28 +0900

    詳細画面完成

---
+++ resources/views/livewire/memos/show.blade.php
@@ -0,0 +1,26 @@
+<?php
+
+use function Livewire\Volt\{state, mount};
+use App\Models\Memo;
+
+state(['memo' => null]);
+
+mount(function (Memo $memo) {
+    $this->memo = $memo;
+});
+
+?>
+
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900">
+                <h1 class="text-2xl font-bold mb-4">{{ $memo->title }}</h1>
+                <div class="text-sm text-gray-500 mb-4">
+                    作成日: {{ $memo->created_at->format('Y年m月d日 H:i') }}
+                </div>
+                <div class="whitespace-pre-wrap">{{ $memo->body }}</div>
+            </div>
+        </div>
+    </div>
+</div>
